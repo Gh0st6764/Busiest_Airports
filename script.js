4791c76019fdae8889050fdb56ef22d22eec2bf7
Gh0st6764 <dpratt28@students.stab.org>
2025-01-16 10:45:00 -0500

    Update script.js
    Signed-off-by: Gh0st6764 <dpratt28@students.stab.org>

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
+//MICHAEL!!!!!!!!!!!
+//I'll do the first to and if I have time I will do three, 
+//I'll work on it tonight, 
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Transportation/Busiest%20Airports.csv";
-var Rank = getColumn(url, 2);
-var Year = getColumn(url, 3);
-var Airport = getColumn(url, 4);
-var Airport_Code = getColumn(url, 5);
-var Airportlocation = getColumn(url, 6);
-var Country = getColumn(url, 7);
-var Passengers = getColumn(url, 8);
-
-// functions could be something like searching for large airports by country?
-// or it could be finding the largest airport averaged over many years?
-
-//thanks for linking every column, 
-//I think it should be searching for the largest airports by country,
-//because if airports are busy they have to be safe and like 
-//good? and it could help people with planning if it has to have
-//a real world affect. 
+var Rank = getColumn(url, 1);
+var Year = getColumn(url, 2);
+var Airport = getColumn(url, 3);
+var Airport_Code = getColumn(url, 4);
+var Airportlocation = getColumn(url, 5);
+var Country = getColumn(url, 6);
+var Passengers = getColumn(url, 7);
 
 //----------------------------------------------------------------------------
 ///This function will search airports on the list from the most recent year(2016),
@@ -22,9 +16,11 @@ var Passengers = getColumn(url, 8);
 function getBusiestAirport(){
     var max = 0;
     var maxAirport = "";
-    for (i < 0; i < Passengers.length; i++){
-        if (Passengers[i] >= max){
-            max = Passengers[i];
+    for (i = 0; i < Passengers.length; i++){
+        console.log(Passengers[i])
+        if (parseFloat(Passengers[i]) > max){
+            console.log("help")
+            max = parseFloat(Passengers[i]);
             maxAirport = Airport[i];
         }
     }
@@ -37,6 +33,21 @@ console.log(getBusiestAirport())
 //This checks all the visiters on the list 
 //and returns the least busiest airport from the year 2016.
 
+function getCalmestAirport(){
+    var min = 1000000000000;
+    var minAirport = "";
+    for (i = 0; i < Passengers.length; i++){
+        // console.log(Passengers[i])
+        if (parseFloat(Passengers[i]) <= min){
+            min = parseFloat(Passengers[i]);
+            minAirport = Airport[i];
+        }
+    }
+
+    return minAirport;
+}
+
+console.log(getCalmestAirport())
 
 //---------------------------------------------------------------------------
 //Find the average total across all airports from 2015-2016
@@ -50,4 +61,3 @@ console.log(getBusiestAirport())
 //---------------------------------------------------------------------------
 //Checks all airports from florida and then prints airports that were busy from 2013-2016
 //THIS SHOULD PRINT ("Miami International Airport", "Orlando International Airport", "Miami International Airport")
-
